bee03e16cbfa4c2189a61fd8452360436aa2035e
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-07-25 17:31:29 +0700

    firebase

---
+++ client/src/apis/firebase.js
@@ -0,0 +1,21 @@
+
+import firebase from 'firebase/firestore'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKjwCC0ShuBRajfZeMZGdA5rvCGhTnZgM",
+  authDomain: "eat-word.firebaseapp.com",
+  databaseURL: "https://eat-word.firebaseio.com",
+  projectId: "eat-word",
+  storageBucket: "",
+  messagingSenderId: "226519008036",
+  appId: "1:226519008036:web:bbdda6fd0ffbd5fe"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+module.exports = db
+
