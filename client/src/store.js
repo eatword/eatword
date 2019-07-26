08f1c72c4c4a9a9058e60642cd1016b007b7bb29
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-07-26 13:23:21 +0700

    leaveRoomDone

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import db from './apis/firebase.js'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 import router from './router.js'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
   state: {
     currentPlayer: '',
@@ -30,12 +31,12 @@ export default new Vuex.Store({
         })
         .then((docs) => {
           console.log(docs)
-          console.log(docs.data())
+          // console.log(docs.data())
           // console.log(docs.id);
           console.log(docs._key.path.segments[1], 'halooooo')
           localStorage.setItem('idRoom', docs._key.path.segments[1])
           router.push(`/waiting/${docs._key.path.segments[1]}`)
-          context.dispatch('getAllRooms')
+          // context.dispatch('getAllRooms')
         })
         .catch((err) => {
           console.log(err)
@@ -64,6 +65,7 @@ export default new Vuex.Store({
       let newPlayers = []
       let room = context.state.rooms.find(room => room.id === roomId)
       newPlayers = room.players
+      console.log(context.state.currentPlayer, 'ini current player')
 
       if (newPlayers.length < 3) {
         newPlayers.push({ name: context.state.currentPlayer, score: 0 })
@@ -98,5 +100,17 @@ export default new Vuex.Store({
           console.error(error)
         })
     },
+    leaveRoom(context,payload) {
+      console.log(context.state.currentPlayer)
+      console.log(localStorage.getItem('username'));
+      db.collection('rooms')
+        .doc(payload)
+        .update({
+          ['players']: ['players'].filter(player => player.name !== localStorage.getItem('username'))
+        })
+        .then(() => {
+          router.push('/lobby')
+        })
+    }
   }
 })
